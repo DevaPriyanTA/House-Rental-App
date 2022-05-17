cce0fe8fde7c5792a21d4ec300c8491601dea0b8
Deva Priyan T A <74890483+DevaPriyanTA@users.noreply.github.com>
2022-05-17 07:37:38 +0530

    Add files via upload

---
+++ constant/product.js
@@ -0,0 +1,56 @@
+const product = [
+    {
+      id: 1,
+      name: 'Joystick',
+      price: '1,499',
+      like: true,
+      img: require('../assets/joystick.png'),
+      about:`Feel Your In-Game Actions and Environment Simulated Through Haptic Feedback
+             Experience Varying Force and Tension at Your Fingertips with Adaptive Triggers
+             Chat Online Through the Built-In Microphone
+             Connect a Headset Directly via the 3.5 mm Jack
+             Switch Voice Capture on and off Using the Dedicated Mute Button
+             Record and Broadcast Your Epic Gaming Moments with the Create Button`,
+    },
+    {
+        id: 2,
+        name: 'Play Station 4',
+        price: '39,999',
+        like: true,
+        img: require('../assets/ps4.png'),
+        about:`
+        Product name    PlayStation®4
+        Product code    CUH-2000 series
+        Main processor  Single-chip custom processor
+                        CPU : x86-64 AMD “Jaguar”, 8 cores
+                        GPU : 1.84 TFLOPS, 
+                        AMD Radeon™ based graphics engine
+        Memory           GDDR5 8GB
+        Storage size*   500GB, 1TB`,
+      },
+      {
+        id: 2,
+        name: 'Curve Display',
+        price: '25,000',
+        like: true,
+        img: require('../assets/Display.png'),
+        about:'',
+      },
+      {
+        id: 2,
+        name: 'Gaming Keyboard',
+        price: '1,400',
+        like: true,
+        img: require('../assets/Keyboard.png'),
+        about:'djsk fsjf jdshf sdhfk dfjfksfhk jhfs ',
+      },
+      {
+        id: 2,
+        name: 'JBL TWS',
+        price: '120.00',
+        like: true,
+        img: require('../assets/JBL.png'),
+        about:'djsk fsjf jdshf sdhfk dfjfksfhk jhfs ',
+      },
+];
+export default product
